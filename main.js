fca0567b13a19dae04a6d9f6afcae0ad2598d45e
Matthijs <stokrom@hva.nl>
2025-02-07 13:18:49 +0100

    Three Js start

---
+++ main.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 2, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+
+scene.add( cube );
+
+scene.background = new THREE.Color( 0x6FA8DC );
+
+camera.position.z = 5;
+
+function animate() {
+
+	cube.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+
+}
