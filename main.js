8e4be937417f8417aa5e00d160557b807cc2dc65
Matthijs <stokrom@hva.nl>
2025-02-07 15:28:09 +0100

    Three Js is life

--- main.js
+++ main.js
@@ -9,19 +9,35 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 2, 1, 1 );
+const geometry = new THREE.BoxGeometry( 5, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+let sword;
+
+const loader = new GLTFLoader();
+
+loader.load( 'Longsword.glb', function ( gltf ) {
+
+    sword = gltf.scene;
+	//scene.add( sword );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 
 scene.add( cube );
 
-scene.background = new THREE.Color( 0x6FA8DC );
+scene.background = new THREE.Color( 0xFF0000);
 
 camera.position.z = 5;
 
 function animate() {
 
-	cube.rotation.y += 0.02;
+	cube.rotation.z += 0.02;
+    cube.rotation.x += 0.02;
+    cube.rotation.y -= 0.02;
 
 	renderer.render( scene, camera );
 
