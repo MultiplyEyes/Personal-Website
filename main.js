2d1f3905ce3785f3dad04dc8007fca4443f5a99f
Matthijs <stokrom@hva.nl>
2025-02-11 10:23:07 +0100

    Updated the way of starting animation and added walking

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function FactsBox()
+function FactBox()
 {
-    
+    //document.getElementById("jump-man").id = "jump-man-fact";
+    var jumpMan = document.querySelector('#jump-man');
+    jumpMan.classList.add('animeermij');
+    sleep(3000).then(() => { jumpMan.classList.remove('animeermij');});
+    var textBox = document.querySelector('#text');
+    sleep(2000).then(() => { textBox.innerHTML = "dasda";});
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
